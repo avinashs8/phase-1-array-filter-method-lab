af98989f31d0115d203ff6392436c19580ce9e36
avinash singh <avinash8.as@gmail.com>
2022-12-04 18:31:47 -0500

    commit tree 6ad06b2a744f422d9bf722d93d9cea037164dff6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+
+function findMatching (arr, name){
+    return arr.filter(function(nam){
+        if (name.toLowerCase() === nam.toLowerCase()){
+          return name
+        }
+    })
+}
+
+// function fuzzyMatch(arr, letter) {
+//     return arr.filter(function(lets) {
+//       if (letter.toLowerCase() === lets[0].toLowerCase()) {
+//         return lets
+//       }
+//     })
+//   }
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+
+  function matchName(arr, nam) {
+    return arr.filter(function(na) {
+      if (na.name === nam) {
+        return nam
+      }
+    })
+  }
